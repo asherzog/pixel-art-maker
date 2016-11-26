36b61329425c0c71fde23ea478f568ad3f3d1e59
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-11-26 15:09:11 -0700

    jquery board

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 $(document).ready(function() {
-  let color = '';
-  let isDown = false;
-  $('.pallete div').click(function() {
-    color = $(this).css('background-color');
-
-    $('.grid div').click(function() {
-      $(this).css('background-color', color);
-    });
-
-    $('.grid div').mousedown(function() {
-      isDown = true;
-      if (isDown) {
-        $(this).css ('background-color', color);
+  $('form').submit(function(event) {
+    event.preventDefault();
+    let $input = $('input').val();
+    for (var i = 0; i < $input; i++) {
+      var row = '<div>';
+      for (var j = 0; j < $input; j++) {
+        row += '<div></div>';
       }
-      $('.grid div').mouseenter(function() {
+      row += '</div>';
+      $('.grid').append(row);
+    }
+    let color = '';
+    let isDown = false;
+    $('.pallete div').click(function() {
+      color = $(this).css('background-color');
+
+      $('.grid div').click(function() {
+        $(this).css('background-color', color);
+      });
+
+      $('.grid div').mousedown(function() {
+        isDown = true;
         if (isDown) {
           $(this).css ('background-color', color);
         }
-      });
-      $('.grid div').mouseup(function() {
-        isDown = false;
+        $('.grid div').mouseenter(function() {
+          if (isDown) {
+            $(this).css ('background-color', color);
+          }
+        });
+        $('.grid div').mouseup(function() {
+          isDown = false;
+        });
       });
     });
-
-
-
     $('button').click(function() {
-      $('.grid div').css('background-color', 'white');
+      $('.grid').html('');
     });
 
   });
